57faa65efacca8bbe97ba60e37d7af0b85ad5383
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-02-18 13:07:28 -0600

    history is working better with no error, need to clean up the look of the site

---
+++ src/components/history/historyInstance.jsx
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+
+export default class HistoryInstance extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    console.log(this.props);
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+
+    let requestParams = {
+      method: this.props.entry.method,
+      url: this.props.entry.url,
+      textArea: this.props.entry.textInput,
+    }
+
+    if (!requestParams.url) {
+      this.props.dispatch({ type: 'SET_ERROR', error: { status: true, message: 'Please input an endpoint URL' } });
+      this.props.dispatch({ type: 'SET_DATA', data: null });
+      return;
+    }
+
+    this.props.dispatch({ type: 'SET_LOADING', loading: true });
+    this.props.dispatch({ type: 'SET_ERROR', error: { status: false } });
+    this.props.dispatch({ type: 'SET_HISTORY', history: requestParams })
+    this.props.dispatch({ type: 'SET_RQST_PARAMS', rqstParams: requestParams });
+  }
+
+  render() {
+    return (
+      <div key={this.props.entry.method} onClick={this.handleClick}>
+        <div name='method' value={this.props.entry.method} >Method: {this.props.entry.method}</div>
+        <div name='url' value={this.props.entry.url} >URL: {this.props.entry.url}</div>
+        <div name='body' value={this.props.entry.body} >Body: {this.props.entry.textArea}</div>
+      </div>
+    )
+  }
+}
